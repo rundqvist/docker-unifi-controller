c94e8395c8f56630f2f7389981f003ca94f8e0c8
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2022-08-31 21:38:44 +0200

    Auto update certificates

---
+++ root/app/certbot/certbot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update certificates
+#
+/usr/sbin/update-ca-certificates -f > /dev/null 2>&1
+
+if [ $? -eq 0 ]
+then
+    echo "$(date) [INF] Certificates updated."
+else
+    echo "$(date) [ERR] Failed to update certificates."
+fi
+
+# Sleep 24h before next update
+#
+sleep 86400
--- root/app/entrypoint.sh
+++ root/app/entrypoint.sh
@@ -5,6 +5,8 @@
 mkdir -p /var/log/{supervisord,unifi}
 mkdir -p /run/supervisord
 
+chmod +x /app/certbot/certbot.sh
+
 # Start supervisord
 #
 exec supervisord -c /app/supervisor/01-supervisord.conf
